70e5f09002762289f11c11d18746fcb729912599
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-11-11 11:22:31 +0100

    Just testing things out

---
+++ api/api.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from utils import system_metrics
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/api')
+def hello_world():
+    return json.dumps({'code': 200, 'message': 'Hello World !', 'author': 'Ibragim Abubakarov'}), 200, {'Content-Type': 'application/json'}
+
+
+@app.route('/api/system_metrics')
+def cpu_metrics():
+    return json.dumps(system_metrics()), 200, {'Content-Type': 'application/json'}
---
+++ api/utils.py
@@ -0,0 +1,34 @@
+from math import floor
+import psutil
+import os
+
+
+def system_metrics():
+    payload = {
+        'cpu_usage': psutil.cpu_percent(),
+    }
+
+    mem_stats = psutil.virtual_memory()
+    payload['memory'] = {
+        'percent': mem_stats.percent,
+        'total': floor(mem_stats.total / (1024 * 1024)),
+        'available': floor(mem_stats.available / (1024 * 1024)),
+        'used': floor(mem_stats.used / (1024 * 1024)),
+        'free': floor(mem_stats.free / (1024 * 1024)),
+        'active': floor(mem_stats.active / (1024 * 1024)),
+        'inactive': floor(mem_stats.inactive / (1024 * 1024)),
+        'buffers': floor(mem_stats.buffers / (1024 * 1024)),
+        'cached': floor(mem_stats.cached / (1024 * 1024)),
+        'shared': floor(mem_stats.shared / (1024 * 1024)),
+        'slab': floor(mem_stats.slab / (1024 * 1024))
+    }
+
+    disk_stats = psutil.disk_usage(os.sep)
+    payload['disk'] = {
+        'percent': disk_stats.percent,
+        'total': round((disk_stats.total / (1024 * 1024)) / 1000, 2),
+        'used': round((disk_stats.used / (1024 * 1024)) / 1000, 2),
+        'free': round((disk_stats.free / (1024 * 1024)) / 1000, 2),
+    }
+
+    return payload
