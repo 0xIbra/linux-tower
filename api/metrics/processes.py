9c2145e388e785a3cac473a71b19cfb8683b3bb9
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-11-21 13:03:28 +0100

    processes - retrieval of cpu usage percent made optional
    It is made optional because it's retrieval takes a bit of time as opposed to retrieving the memory usage.

--- api/metrics/processes.py
+++ api/metrics/processes.py
@@ -1,22 +1,26 @@
 import psutil
 
 
-def processes_info():
+def processes_info(with_cpu_usage=False):
     payload = []
     for proc in psutil.process_iter(['pid', 'username', 'name']):
-        proc.cpu_percent()
-        cpu_usage = proc.cpu_percent(interval=0.1)
-        if cpu_usage > 0.0:
-            cpu_usage = cpu_usage / psutil.cpu_count()
-        else:
-            cpu_usage = None
+        cpu_usage = None
+        if with_cpu_usage is True:
+            proc.cpu_percent()
+            cpu_usage = proc.cpu_percent(interval=0.1)
+            if cpu_usage > 0.0:
+                cpu_usage = cpu_usage / psutil.cpu_count()
 
         payload.append({
             'name': proc.name(),
             'pid': proc.pid,
             'user': proc.username(),
             'usage': {
-                'cpu': cpu_usage
+                'cpu': cpu_usage,
+                'memory': {
+                    'percent': round(proc.memory_percent(), 2),
+                    'rss': proc.memory_info().rss / (1024 * 1024)
+                }
             }
         })
 
