f50218aff3001f5d8182790280ee6c77f3230b30
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-12-10 17:53:57 +0100

    Alerting background job - every 60 seconds

--- api/wsgi.py
+++ api/wsgi.py
@@ -2,14 +2,14 @@ from api import create_app
 from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
 
-app = create_app()
 
+app = create_app()
 
 if __name__ == '__main__':
     from background.alerting import alerting_task
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(id='alerting', func=alerting_task, args=[app], trigger='interval', seconds=10)
+    scheduler.add_job(id='alerting', func=alerting_task, args=[app], trigger='interval', seconds=60)
     scheduler.start()
     atexit.register(lambda: scheduler.shutdown())
 
