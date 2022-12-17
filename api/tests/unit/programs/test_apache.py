9659166f9665e17ce2e8deb029c7929ae44bc3f3
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-12-17 13:28:00 +0100

    Apache class tests

---
+++ api/tests/unit/programs/test_apache.py
@@ -0,0 +1,52 @@
+from helpers.programs import Apache
+
+
+def test_is_installed():
+    """
+    GIVEN a 'Apache' class that groups together methods relevant to the Apache systemd service
+    WHEN 'is_installed' method is called
+    THEN check that the returned value is true
+    """
+
+    is_installed = Apache.is_installed()
+    assert is_installed is True
+
+
+def test_status():
+    """
+    GIVEN a 'Apache' class that groups together methods relevant to the Apache systemd service
+    WHEN 'status' method is called
+    THEN check that the returned value contains text saying apache2 is running
+    """
+
+    status = Apache.status()
+    assert type(status) is str
+    assert 'active (running) since' in status
+
+
+def test_service_show():
+    """
+    GIVEN a 'Apache' class that groups together methods relevant to the Apache systemd service
+    WHEN 'service_show' method is called
+    THEN check that the returned value is dict and contains relevant information
+    """
+
+    service_data = Apache.service_show()
+    assert type(service_data) is dict
+    assert 'SubState' in service_data
+    assert service_data['SubState'] == 'running'
+
+
+def test_state_details():
+    """
+    GIVEN a 'Apache' class that groups together methods relevant to the Apache systemd service
+    WHEN 'state_details' method is called
+    THEN check that the returned value is dict and contains formatted data
+    """
+
+    state_details = Apache.state_details()
+    assert type(state_details) is dict
+    assert 'id' in state_details
+    assert 'description' in state_details
+    assert state_details['state'] == 'running'
+    assert 'started_at' in state_details
