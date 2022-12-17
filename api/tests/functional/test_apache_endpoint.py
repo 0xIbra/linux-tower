3d0abc84eeffe43c9a15942c7accc860c8d0e5a4
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-12-17 22:42:06 +0100

    Apache status endpoint tested

---
+++ api/tests/functional/test_apache_endpoint.py
@@ -0,0 +1,18 @@
+def test_apache_endpoint(testing_app, access_token):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN a GET request is made to '/api/apache'
+    THEN check that apache state data is returned with a 200 http response
+    """
+
+    with testing_app.test_client() as client:
+        response = client.get('/api/apache/status', headers={'Authorization': access_token})
+        content = response.get_json()
+
+        assert response.status_code == 200
+        assert response.headers.get('content-type') == 'application/json'
+        assert type(content) is dict
+        assert 'data' in content
+        assert 'status' in content['data']
+        assert 'details' in content['data']
+        assert content['data']['details']['state'] == 'running'
