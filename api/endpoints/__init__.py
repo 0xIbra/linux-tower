971daf9e133dd09a4d57af3963cdf69fa9d92dd6
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-12-18 02:19:06 +0100

    removing log_files entity - no longer useful
    entity no longer useful because now the api is protected by authentication.

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -3,6 +3,5 @@ from flask import Blueprint
 blueprint = Blueprint('api_blueprint', __name__)
 
 from endpoints import system_metrics, processes, apache, nginx, log_reader
-from endpoints.log_files import *
 from endpoints.alerts import *
 from endpoints.auth import *
