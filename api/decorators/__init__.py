776c29f6f435f3d9f1f4be9f003ecf5f65b378f3
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-12-18 02:53:06 +0100

    auth decorator improved

--- api/decorators/__init__.py
+++ api/decorators/__init__.py
@@ -12,33 +12,33 @@ def is_authenticated(f):
             token = request.headers['authorization']
 
         if not token:
-            return jsonify({'message': 'access token not found in request headers[authorization].'})
+            return jsonify({'detail': 'access token not found in request headers[authorization].'}), 401
 
         try:
             jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
         except:
-            return jsonify({'message': 'access token is invalid.'}), 401
+            return jsonify({'detail': 'access token is invalid.'}), 401
 
         return f(*args, **kwargs)
 
     return decorator
 
 
-def is_authenticated_with_user(f):
-    @wraps(f)
-    def decorator(*args, **kwargs):
-        token = None
-        if 'authorization' in request.headers:
-            token = request.headers['authorization']
-
-        if not token:
-            return jsonify({'message': 'access token not found in request headers[authorization].'})
-
-        try:
-            user = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
-        except:
-            return jsonify({'message': 'access token is invalid.'})
-
-        return f(user, *args, **kwargs)
-
-    return decorator
+# def is_authenticated_with_user(f):
+#     @wraps(f)
+#     def decorator(*args, **kwargs):
+#         token = None
+#         if 'authorization' in request.headers:
+#             token = request.headers['authorization']
+#
+#         if not token:
+#             return jsonify({'message': 'access token not found in request headers[authorization].'})
+#
+#         try:
+#             user = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+#         except:
+#             return jsonify({'message': 'access token is invalid.'})
+#
+#         return f(user, *args, **kwargs)
+#
+#     return decorator
