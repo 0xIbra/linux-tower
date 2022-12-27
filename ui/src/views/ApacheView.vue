55f463d8f2c4bf23d0ac7d5d118d90183085ec3f
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-12-27 07:26:22 +0100

    UI - apache view visual changes

--- ui/src/views/ApacheView.vue
+++ ui/src/views/ApacheView.vue
@@ -46,7 +46,7 @@ const updateLogs = () => {
     <section v-if="apacheStore.data != null">
       <div class="container-fluid">
         <div class="row gy-4">
-          <div class="col-md-4 col-sm-6">
+          <div class="col-md-3 col-sm-6">
             <div class="row gy-4">
               <div class="col-md-12">
                 <div class="cpu-usage-wrapper card mb-0">
@@ -62,61 +62,45 @@ const updateLogs = () => {
               </div>
 
               <div class="col-md-12">
-                <div class="cpu-usage-wrapper card mb-0">
+                <div v-if="apacheStore.metrics != null" class="cpu-usage-wrapper card mb-0">
                   <div class="card-body">
-                    <div class="me-2 mb-2">
-                      <p class="text-sm text-gray-600 mt-2 mb-0">Status logs:</p>
+                    <div class="d-flex align-items-end justify-content-between mb-2">
+                      <div class="me-2">
+                        <img class="svg-icon svg-icon-sm svg-icon-heavy text-gray-600 mb-2" src="icons/processor-32.png" />
+                        <p class="text-sm text-uppercase text-gray-600 lh-1 mb-0">CPU Usage</p>
+                      </div>
+                      <p class="text-xxl lh-1 mb-0" :class="metricTextColorClass(apacheStore.metrics.cpu_usage)" >{{ apacheStore.metrics.cpu_usage }}%</p>
                     </div>
-                    <Logs v-if="apacheStore.data != null" :raw-logs="apacheStore.data.status" :display-lines="false" />
+                    <div class="progress" style="height: 3px">
+                      <div class="progress-bar" :class="metricBgColorClass(apacheStore.metrics.cpu_usage)" role="progressbar" :style="{width: Math.ceil(apacheStore.metrics.cpu_usage).toString() + '%'}" :aria-valuenow="parseInt(apacheStore.metrics.cpu_usage).toString()" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-12">
+                <div v-if="apacheStore.metrics != null" class="card mb-0">
+                  <div class="card-body">
+                    <div class="d-flex align-items-end justify-content-between mb-2">
+                      <div class="me-2">
+                        <svg class="svg-icon svg-icon-sm svg-icon-heavy text-gray-600 mb-2">
+                          <use xlink:href="#stack-1"></use>
+                        </svg>
+                        <p class="text-sm text-uppercase text-gray-600 lh-1 mb-0">Memory</p>
+                      </div>
+                      <p class="text-xxl lh-1 mb-0" :class="metricTextColorClass(apacheStore.metrics.memory.percent)">{{ parseInt(apacheStore.metrics.memory.used).toString() }} MB</p>
+                    </div>
+                    <div class="progress" style="height: 3px">
+                      <div class="progress-bar" :class="metricBgColorClass(apacheStore.metrics.memory.percent)" role="progressbar" :style="{width: parseInt(apacheStore.metrics.memory.percent).toString() + '%'}" :aria-valuenow="parseInt(apacheStore.metrics.memory.percent).toString()" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="col-md-6">
+          <div class="col-md-7">
             <div class="row gy-4">
-              <div class="col-12">
-                <div class="row">
-                  <div class="col-6">
-
-                    <div v-if="apacheStore.metrics != null" class="cpu-usage-wrapper card mb-0">
-                      <div class="card-body">
-                        <div class="d-flex align-items-end justify-content-between mb-2">
-                          <div class="me-2">
-                            <img class="svg-icon svg-icon-sm svg-icon-heavy text-gray-600 mb-2" src="icons/processor-32.png" />
-                            <p class="text-sm text-uppercase text-gray-600 lh-1 mb-0">CPU Usage</p>
-                          </div>
-                          <p class="text-xxl lh-1 mb-0" :class="metricTextColorClass(apacheStore.metrics.cpu_usage)" >{{ apacheStore.metrics.cpu_usage }}%</p>
-                        </div>
-                        <div class="progress" style="height: 3px">
-                          <div class="progress-bar" :class="metricBgColorClass(apacheStore.metrics.cpu_usage)" role="progressbar" :style="{width: Math.ceil(apacheStore.metrics.cpu_usage).toString() + '%'}" :aria-valuenow="parseInt(apacheStore.metrics.cpu_usage).toString()" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-6">
-                    <div v-if="apacheStore.metrics != null" class="card mb-0">
-                      <div class="card-body">
-                        <div class="d-flex align-items-end justify-content-between mb-2">
-                          <div class="me-2">
-                            <svg class="svg-icon svg-icon-sm svg-icon-heavy text-gray-600 mb-2">
-                              <use xlink:href="#stack-1"></use>
-                            </svg>
-                            <p class="text-sm text-uppercase text-gray-600 lh-1 mb-0">Memory</p>
-                          </div>
-                          <p class="text-xxl lh-1 mb-0" :class="metricTextColorClass(apacheStore.metrics.memory.percent)">{{ parseInt(apacheStore.metrics.memory.used).toString() }} MB</p>
-                        </div>
-                        <div class="progress" style="height: 3px">
-                          <div class="progress-bar" :class="metricBgColorClass(apacheStore.metrics.memory.percent)" role="progressbar" :style="{width: parseInt(apacheStore.metrics.memory.percent).toString() + '%'}" :aria-valuenow="parseInt(apacheStore.metrics.memory.percent).toString()" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                </div>
-              </div>
               <div class="col-12">
                 <div class="cpu-usage-wrapper card mb-0">
                   <div class="card-body">
@@ -131,6 +115,20 @@ const updateLogs = () => {
                   </div>
                 </div>
               </div>
+
+              <div class="col-12">
+                <div class="col-md-12">
+                  <div class="cpu-usage-wrapper card mb-0">
+                    <div class="card-body">
+                      <div class="me-2 mb-2">
+                        <p class="text-sm text-gray-600 mt-2 mb-0">Status logs:</p>
+                      </div>
+                      <Logs v-if="apacheStore.data != null" :raw-logs="apacheStore.data.status" :display-lines="false" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
             </div>
           </div>
         </div>
