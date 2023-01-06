ecda05c421fe8f634b8711560e1d4ece5bf7716d
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2023-01-06 08:02:19 +0100

    UI - logs view - improvements - more work to be done

--- ui/src/views/LogsView.vue
+++ ui/src/views/LogsView.vue
@@ -10,9 +10,11 @@ const route = useRoute();
 
 const authStore = useAuthStore();
 
+const liveTail = ref(false);
 const logFilePath = ref("/var/log/syslog");
 const filterRegex = ref("");
 const logs = ref([]);
+const logFileError = ref("");
 
 const tailLogs = async () => {
   const response = await axios.request({
@@ -38,7 +40,7 @@ const queryLogs = async () => {
     headers: { Authorization: authStore.accessToken },
   });
 
-  return response.data['logs'];
+  return response.data["logs"];
 };
 
 const render = () => {
@@ -48,11 +50,21 @@ const render = () => {
 
 const update = async (e: any) => {
   e.preventDefault();
+  logs.value = [];
+  logFileError.value = "";
 
-  if (filterRegex.value != "" && filterRegex.value != null) {
-    logs.value = await queryLogs();
-  } else {
-    logs.value = await tailLogs();
+  if (liveTail.value === true) {
+    return;
+  }
+
+  try {
+    if (filterRegex.value != "" && filterRegex.value != null) {
+      logs.value = await queryLogs();
+    } else {
+      logs.value = await tailLogs();
+    }
+  } catch (e: any) {
+    logFileError.value = e.response.data["detail"];
   }
 };
 
@@ -62,16 +74,10 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="page-content form-page">
+  <div class="page-content form-page h-100">
     <form @submit="update">
-      <!-- Page Header-->
-      <div class="bg-dash-dark-2 py-4">
-        <div class="container-fluid">
-          <h2 class="h5 mb-0">Alerts</h2>
-        </div>
-      </div>
       <!-- Breadcrumb-->
-      <div class="container-fluid py-2">
+      <div class="container-fluid py-2 h-100">
         <div class="flex justify-content-space-between">
           <nav aria-label="breadcrumb">
             <ol class="breadcrumb mb-0 py-3">
@@ -92,12 +98,12 @@ onMounted(async () => {
                   <div class="card">
                     <div class="card-body pt-0 modernize">
                       <div class="input-group">
-                        <span class="input-group-text" id="basic-addon1">
+                        <span class="input-group-text" id="file-field-icon">
                           <svg class="svg-icon svg-icon-sm">
-                            <use xlink:href="#document-saved-1"></use>
+                            <use xlink:href="#literature-1"></use>
                           </svg>
                         </span>
-                        <input v-model="logFilePath" class="form-control modernize h-modernize" type="text" placeholder="search for logs using regex" />
+                        <input v-model="logFilePath" class="form-control modernize h-modernize" :class="{'is-invalid': logFileError != ''}" type="text" placeholder="search for logs using regex" />
                       </div>
                     </div>
                   </div>
@@ -107,7 +113,11 @@ onMounted(async () => {
                   <div class="card mt-2">
                     <div class="card-body pt-0 modernize">
                       <div id="logs-wrapper">
-                        <p v-for="logItem in logs" class="log-text">{{ logItem.log.replace(/(.{230})..+/, "$1&hellip;") }}</p>
+                        <p v-for="logItem in logs" class="log-text">{{ logItem.log.replace(/(.{220})..+/, "$1&hellip;") }}</p>
+                      </div>
+                      <div v-if="liveTail" class="text-center p-0 m-0 pt-1 pb-1 bg-color-primary text-white">
+                        waiting on live logs...
+                        <span class="spinner"></span>
                       </div>
                     </div>
                   </div>
@@ -116,7 +126,7 @@ onMounted(async () => {
             </div>
 
             <div class="col-lg-2">
-              <div class="row">
+              <div class="row sticky-top">
                 <div class="col-12">
                   <div class="card mb-0">
                     <div class="card-body p-0">
@@ -127,7 +137,13 @@ onMounted(async () => {
 
                 <div class="col-12 mt-2">
                   <div class="flex">
-                    <button type="submit" class="btn btn-primary w-100">Query</button>
+                    <a class="btn w-100" :class="{'btn-outline-secondary': !liveTail, 'btn-secondary': liveTail}" @click="liveTail = !liveTail">Live tail</a>
+                  </div>
+                </div>
+
+                <div class="col-12 mt-2">
+                  <div class="flex">
+                    <button type="submit" class="btn btn-primary w-100" v-bind:disabled="liveTail">Query</button>
                   </div>
                 </div>
               </div>
@@ -140,9 +156,19 @@ onMounted(async () => {
 </template>
 
 <style scoped>
+html, body {
+  height: 100%;
+}
+#file-field-icon {
+  background: #1b1e22 !important;
+  border: none !important;
+}
+
 #logs-wrapper {
   padding: 10px 10px !important;
   font-family: "Arial", sans-serif !important;
+  height: 72vh;
+  overflow-y: scroll;
 }
 
 .log-text {
@@ -160,7 +186,7 @@ onMounted(async () => {
   background: #353941;
 }
 
-input.modernize {
+input.modernize:not(.is-invalid) {
   background: #22252a !important;
   border: 0 !important;
 }
@@ -196,5 +222,18 @@ input.h-modernize {
 }
 
 ::-webkit-scrollbar-corner {
+}
+
+.btn-primary[disabled] {
+  background: #589b83 !important;
+  border-color: #589b83 !important;
+}
+
+.sticky-top {
+  top: 20px !important;
+}
+
+button:focus, input:focus {
+  box-shadow: none !important;
 }
 </style>
